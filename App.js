bfc6045f21f93519e222347252abb7ca27465308
sunilkumar_seenivasan <100457242+sunil52sk@users.noreply.github.com>
2023-03-27 12:04:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+
+const name1="Kabil";
+const name2="Kabil";
+const object1={name:"Kabil"};
+const object2={name:"Kabil"};
+function App() {
+    console.log(name2===name1);
+    console.log(object1===object2);
+}
